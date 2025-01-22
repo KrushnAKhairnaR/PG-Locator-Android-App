0c1df841dda5bf7ee6c561b0e4a38f7661ff1e42
Krushna Khairnar <krushnakhairnar11work@gmail.com>
2025-01-22 12:32:44 +0530

    All Files

---
+++ PGAdmin and pg locator/PG Locator App/app/src/main/java/com/dd/pglocator/PaymentActivity.java
@@ -0,0 +1,15 @@
+package com.dd.pglocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        getSupportActionBar().hide();
+    }
+}
